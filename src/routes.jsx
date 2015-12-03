32160e48b0fb6d858c237c111bc3369d53cddea2
Blackstock <charlieblackstock@gmail.com>
2015-12-03 09:46:07 -0700

    updated routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -11,7 +11,7 @@ var ItemDetail = require('./components/item-detail');
 module.exports = (
   <Router history={history()}>
     <Route path="/" component={Main}>
-      <Route path="/item/:id" component={ItemDetail} />
+      <Route path="item/:id" component={ItemDetail} />
     </Route>
   </Router>
 );
