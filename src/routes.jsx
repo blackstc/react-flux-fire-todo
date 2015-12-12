bd5bc2c4c0643b3b86055d78f95e53be18f5e9e4
Blackstock <charlieblackstock@gmail.com>
2015-12-12 10:16:40 -0700

    updated routes for todos

--- src/routes.jsx
+++ src/routes.jsx
@@ -7,8 +7,8 @@ var Route = ReactRouter.Route;
 //route components
 var Main = require('./components/main');
 var ItemDetail = require('./components/item-detail');
-
 module.exports = (
+
   <Router history={history()}>
     <Route path="/" component={Main}>
       <Route path="item/:id" component={ItemDetail} />
